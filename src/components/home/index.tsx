12e3cce2459e60457fc28735c115f8f1001fae78
herom <jerome.manset22@gmail.com>
2023-04-12 06:06:58 +0800

    Add animation to the home page text using framer motion

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,6 +2,8 @@ import { SelectedPage } from '@/shared/types'
 import ActionButton from '@/shared/ActionButton'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import HomeGraphic from '@/assets/image/HomeGraphic.jpg'
+import { motion } from 'framer-motion'
+
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -10,11 +12,31 @@ type Props = {
 const Home = ({setSelectedPage}: Props) => {
   return (
     <section id='home' className='mt-32'>
-        <div className='sm:flex w-5/6 mx-auto items-center justify-between'>
-            <div className='text-center flex flex-col items-center gap-4 mb-4 sm:mb-0 sm:basis-3/5'>
+        <motion.div 
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        className='sm:flex w-5/6 mx-auto items-center justify-between'>
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5}} 
+            transition={{duration: 1}}
+            variants={{
+                hidden: {opacity: 0, x:-100},
+                visible: {opacity: 1, x:0}
+            }}
+            className='text-center flex flex-col items-center gap-4 mb-4 sm:mb-0 sm:basis-3/5'>
                 <h1 className='font-anton text-4xl sm:text-6xl sm:leading-tight max-w-sm'>Welcome to the <span className='whitespace-nowrap'>Elite Ballers</span> <span className='whitespace-nowrap'>Training Camp</span> </h1>
                 <p>Here you'll find everything you need to take your basketball skills to the next level.</p>
-                <div className='flex items-center gap-4 text-sm'>
+                <motion.div 
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true, amount: 0.5}} 
+                transition={{delay: 0.2, duration: 1}}
+                variants={{
+                    hidden: {opacity: 0, x:-100},
+                    visible: {opacity: 1, x:0}
+                }}
+                className='flex items-center gap-4 text-sm'>
                     <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                     <AnchorLink 
                         className='text-primary-orange font-bold underline'
@@ -22,12 +44,12 @@ const Home = ({setSelectedPage}: Props) => {
                         href={`#${SelectedPage.ContactUs}`}>
                         <p>Learn More</p>
                     </AnchorLink>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
             <div className='sm:w-[300px]'>
                 <img className='rounded-lg' src={HomeGraphic} alt="home-graphic" />
             </div>
-        </div>
+        </motion.div>
     </section>
   )
 }
